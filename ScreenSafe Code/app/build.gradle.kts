80e8fb070c50c0391051eb33d862bc3cdc07edb8
kdnwarren <kathleen.neff-warr@email.saintleo.edu>
2026-04-20 12:33:20 -0400

    Update build.gradle.kts
    Update build.gradle.kts for version 1.1.0 of app code

--- ScreenSafe Code/app/build.gradle.kts
+++ ScreenSafe Code/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
-    namespace = "com.example.safescreen"
+    namespace = "com.example.screensafe"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.safescreen"
+        applicationId = "com.example.screensafe"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -34,5 +35,14 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
     implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core:1.13.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.2")
 }
